19b985950f19d0b0a146a6224b1bcec897146795
KietLe624 <kietle06022004@gmail.com>
2023-11-18 21:44:52 +0700

    Add project files.

---
+++ DKFuni_MVC/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DKFuni_MVC.Models;
+namespace DKFuni_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            DKSHOP_DBEntities dBContext = new DKSHOP_DBEntities();
+            List<Product_Category> Listproduct = dBContext.Product_Category.ToList();
+            return View(Listproduct);
+        }
+    }
+}
---
+++ DKFuni_MVC/Controllers/ShopController.cs
@@ -0,0 +1,26 @@
+﻿using DKFuni_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DKFuni_MVC.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Shop()
+        {
+            DKSHOP_DBEntities dbContect = new DKSHOP_DBEntities();
+            List<Product_Category> Listproduct = dbContect.Product_Category.ToList();
+            return View(Listproduct);
+        }
+        public ActionResult Detail(int id)
+        {
+            DKSHOP_DBEntities dbContect = new DKSHOP_DBEntities();
+            Product_Category product = new Product_Category();
+            return View(product);
+        }
+    }
+}
